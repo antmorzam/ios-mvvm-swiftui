4bdb5dd94932d9edf52f9f911a55759ea06423cc
Antonio Moreno <>
2024-02-27 09:50:25 +0100

    Create the project base

---
+++ ios-mvvm-swiftui/ios-mvvm-swiftui/ios_mvvm_swiftuiApp.swift
@@ -0,0 +1,20 @@
+//
+//  ios_mvvm_swiftuiApp.swift
+//  ios-mvvm-swiftui
+//
+//  Created by Antonio Moreno on 27/2/24.
+//
+
+import SwiftUI
+
+@main
+struct ios_mvvm_swiftuiApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
